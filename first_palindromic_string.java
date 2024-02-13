c9480251c8d8a5755a31ee17c8f0e33c9c84f168
Kalebe Szlachta <39911310+kalebers@users.noreply.github.com>
2024-02-13 17:12:30 +0100

    Create first_palindromic_string.java

---
+++ first_palindromic_string.java
@@ -0,0 +1,29 @@
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+// A string is palindromic if it reads the same forward and backward.
+
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
