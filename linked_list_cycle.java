1c9abe4373603a26d6ac8303a29c8e87476d89b2
Kalebe Szlachta <39911310+kalebers@users.noreply.github.com>
2024-03-06 08:49:56 +0100

    Create linked_list_cycle.java

---
+++ linked_list_cycle.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false; // If there is no cycle, the fast pointer will reach the end of the list
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true; // If there is a cycle, the pointers will eventually meet
+    }
+}
